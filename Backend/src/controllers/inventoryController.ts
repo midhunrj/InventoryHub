5b177dec18e215929a492e00a0dce05b97b05aef
midhunrj <midhunrj18852@gmail.com>
2025-01-28 17:32:24 +0530

    final showdown

--- Backend/src/controllers/inventoryController.ts
+++ Backend/src/controllers/inventoryController.ts
@@ -18,6 +18,8 @@ export class InventoryController{
         const {refreshToken}=user
         res.cookie('RefreshToken', refreshToken, {
             httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', // Use HTTPS in production
+    sameSite: 'none', 
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
         
